62e0bfbe6bff969198d32a470052eaea8224de9d
= <=>
2022-06-11 14:32:57 -0300

    Inicio de criação da Lógica

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Teste");
+//manipulando a calculadora através do dom
+let display = document.querySelector('span');
+let container = document.getElementById('container');
+number1 =container.childNodes[13];
+
+function capturaClick(){
+    addEventListener('click', function(){
+        display.innerText= number1.innerHTML;
+    })
+}
+capturaClick();
+console.log(number1.content);
+
+
